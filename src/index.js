cc9fd062ddbfb13354f555d792a21fe7ace3d721
italofelipe <felipe.italo@hotmail.com>
2023-02-21 20:18:53 -0300

    chore: file renamed from .ts to .js due to a bug at build time

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import("./bootstrap");
+/* For an unknown reason, this file cannot be .ts. This error happens probably at build
+time, since it doesn't crashes webpack, but instead, crashes directly in the browser 
+*/
--- src/index.ts
+++
@@ -1,6 +0,0 @@
-console.log("Container app")
-
-const foo = (): void => {
-  console.log("Foo é Typescript")
-}
-foo()
