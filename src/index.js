f6271bdca27360a814ac0cd10124a6722a9b5d74
italofelipe <felipe.italo@hotmail.com>
2023-02-23 22:46:29 -0300

    chore: added entrypoint for cart mfe

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-import("./bootstrap");
+import("./bootstrap")
+  .then(mfe => console.log("O que retorna", mfe))
+  .catch(err => console.log("Erro", err));
 /* For an unknown reason, this file cannot be .ts. This error happens probably at build
 time, since it doesn't crashes webpack, but instead, crashes directly in the browser 
 */
